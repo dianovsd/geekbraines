53284330ac40a6d057d36730fda7477230cdb417
dianovsd <dianovsd@yandex.ru>
2023-04-10 17:00:54 +0400

    phonebook updated

--- python/homework/seminar_8/1.py
+++ python/homework/seminar_8/1.py
@@ -1,20 +1,75 @@
-file_path = 'text.txt'
+file_path = 'phonebook.txt'
 
 def read_app():
-    with open(file_path,'a') as f:
+    with open(file_path,'r ') as f:
         print(f.read())
 
 def append_app():
     with open(file_path, 'a') as f:
-        f.write('\n' +input())
+        f.write('\n' +input("введите новые ФИО и номер телефона "))
 
 def find_app():
-    temp = input('Введите слово для поиска: ')
+    temp = input('Кто вас интересует: ')
     with open(file_path, 'r') as f:
         for line in f:
-            if temp in line:
-                print (line.strip())
+            if temp.lower() in line.lower():
+                return (line.strip())
 
-# def rewrite_app():
-#      with open(file_path, 'w') as f:
-         
+def delete_app():
+    with open(file_path,'r') as f:
+        lines = f.readlines()
+    text = find_app()
+    with open(file_path,'w') as f:
+        for line in lines:
+            if line.strip() != text:
+                f.write(line)
+
+def change_app():
+    delete_app()
+    append_app()
+
+
+
+import tkinter as tk
+from tkinter import Text
+
+root = tk.Tk()
+root.title("Телефонная книга")
+
+frame1 = tk.Frame(root)
+frame1.pack(side=tk.TOP)
+
+text = open('phonebook.txt', encoding='utf-8').readlines()
+text = ''.join(text)
+textline = Text(frame1)
+textline.insert(1.0, text)
+textline.pack(side=tk.LEFT)
+
+button = tk.Button(frame1, 
+                   text="Найти контакт", 
+                   fg="red",
+                   command=find_app)
+button.pack(side=tk.LEFT)
+
+button = tk.Button(frame1, 
+                   text="Добавить контакт", 
+                   fg="red",
+                   command=append_app)
+button.pack(side=tk.LEFT)
+
+frame2 = tk.Frame(root)
+frame2.pack(side=tk.BOTTOM)
+
+button = tk.Button(frame2, 
+                   text="Изменить контакт", 
+                   fg="red",
+                   command=change_app)
+button.pack(side=tk.LEFT)
+
+button = tk.Button(frame2, 
+                   text="Удалить контакт", 
+                   fg="red",
+                   command=delete_app)
+button.pack(side=tk.LEFT)
+
+root.mainloop()
--- python/other/work.py
+++ python/other/work.py
@@ -1,1 +1,13 @@
-import this
+import tkinter as tk
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="Hello World", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.RIGHT)
+
+root.mainloop()
