9ab54a7a88f9b4de41e359875fde95c5493a49fb
dianovsd <dianovsd@yandex.ru>
2023-04-11 16:46:09 +0400

    1 seminar C#

--- c#/seminar/Seminar_1/task_01/Program.cs
+++ c#/seminar/Seminar_1/task_01/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine ($"Квадрат равен = {square}");
---
+++ c#/seminar/Seminar_1/task_02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+{
+if (number1 == number2 * number2)
+    Console.WriteLine("YES!");
+else Console.WriteLine("NO!");
+}
