ecf3a4c212ed3be99a2f085eda6f7bae019657e9
dianovsd <dianovsd@yandex.ru>
2023-08-21 16:14:13 +0400

    new array from user

--- Final_task_digital_master/program/Program.cs
+++ Final_task_digital_master/program/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿static void Main(string[] args)
+{
+    Console.WriteLine("Введите размер исходного массива строк:");
+    int size = int.Parse(Console.ReadLine());
+    string[] array = new string[size];
+    Console.WriteLine("Введите элементы исходного массива строк:");
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
