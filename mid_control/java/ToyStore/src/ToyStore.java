c2d8dbe3845a1b22d1f9d4253eef72dd0ad230d1
dianovsd <dianovsd@yandex.ru>
2023-10-16 11:23:29 +0400

    java mid_control_ver2

--- mid_control/java/ToyStore/src/ToyStore.java
+++ mid_control/java/ToyStore/src/ToyStore.java
@@ -6,12 +6,18 @@ import java.util.*;
 class ToyStore {
     List<Toy> toys = new ArrayList<>();
     Queue<Toy> prizeToys = new LinkedList<>();
+    int drawCount = 0;
 
     void addToy(Toy toy) {
         toys.add(toy);
     }
 
-    void organizeDraw() {
+    void organizeDraw() throws IOException {
+        drawCount++;
+        BufferedWriter writer = new BufferedWriter(new FileWriter("prizes.txt", true));
+        writer.append("Розыгрыш #" + drawCount + "\n");
+        writer.close();
+
         Random rand = new Random();
         double p = rand.nextDouble() * 100.0;
         double cumulativeProbability = 0.0;
@@ -37,4 +43,5 @@ class ToyStore {
         for (Toy toy : toys) {
             System.out.println("ID: " + toy.id + ", Name: " + toy.name + ", Quantity: " + toy.quantity + ", Weight: " + toy.weight);
         }
-    }}
+    }
+}
